f7877314fed580e280119cf1d9f59378dde4fa80
RubensRafael <rubensrafael2@live.com>
2022-10-08 12:12:55 -0300

    change url

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './components/store';
-import Routes from './routes';
-import { Container } from '@material-ui/core/';
-import Header from './components/Header';
-import './global.css'
-
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./components/store";
+import Routes from "./routes";
+import { Container } from "@material-ui/core/";
+import Header from "./components/Header";
+import "./global.css";
+import { useEffect } from "react";
 
 const App = () => {
-  
-  const localCart = JSON.parse(localStorage.getItem('dioshopping: cart'))
-  
-  if(localCart !== null) {
-    store.dispatch({type: 'CHANGE_CART', localCart})
+  const localCart = JSON.parse(localStorage.getItem("dioshopping: cart"));
+
+  if (localCart !== null) {
+    store.dispatch({ type: "CHANGE_CART", localCart });
   }
-  
-  return(
+  useEffect(() => {
+    try {
+      fetch("https://dio-shop.onrender.com");
+    } catch {
+      //
+    }
+  }, []);
+  return (
     <Provider store={store}>
-    
       <Container maxWidth="xl">
         <Router>
           <Header />
           <Routes />
         </Router>
-      </Container> 
-     
+      </Container>
     </Provider>
-  )
-}
+  );
+};
 
 export default App;
--- src/Pages/contato.js
+++ src/Pages/contato.js
@@ -1,98 +1,131 @@
-import { useState, useEffect } from 'react';
-import { Grid, Button, TextField } from '@material-ui/core/';
+import { useState, useEffect } from "react";
+import { Grid, Button, TextField } from "@material-ui/core/";
 
 const Contatos = () => {
+  const url = "https://dio-shop.onrender.com/message";
+  const [message, setMessage] = useState([]);
+  const [author, setAuthor] = useState("");
+  const [content, setContent] = useState("");
+  const [validator, setValidator] = useState(false);
+  const [render, setRender] = useState(false);
+  const [success, setSuccess] = useState(false);
 
-    const url = 'https://dioshopping.herokuapp.com/message'
-    const [message, setMessage] = useState([]);
-    const [author, setAuthor] = useState('');
-    const [content, setContent] = useState('');
-    const [validator, setValidator] = useState(false);
-    const [render, setRender] = useState(false);
-    const [success, setSuccess] = useState(false);
+  useEffect(async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setMessage(data);
+  }, [render]);
 
-    useEffect(async () => {
-        const response = await fetch(url)
-        const data = await response.json();
-        setMessage(data);
-    }, [render])
+  const sendMessage = () => {
+    setValidator(false);
+    if (author.length <= 0 || content.length <= 0) {
+      return setValidator(!validator);
+    }
+    const bodyForm = {
+      email: author,
+      message: content,
+    };
 
-    const sendMessage = () => {
-        setValidator(false);
-        if(author.length <= 0 || content.length <= 0){
-            return setValidator(!validator)
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bodyForm),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.id) {
+          setRender(true);
+          setSuccess(true);
+          setTimeout(() => {
+            setSuccess(false);
+          }, 5000);
         }
-        const bodyForm = {
-            email: author,
-            message: content,
-        }
+      });
 
-        fetch(url, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(bodyForm)
-        })
-        .then((response) => response.json())
-        .then((data) => {
-            if(data.id) {
-                setRender(true);
-                setSuccess(true);
-                setTimeout(() => {
-                    setSuccess(false);
-                }, 5000)
-            }
-        })
-        
-        setAuthor('');
-        setContent('');
-        
-        console.log(content)
-    }  
+    setAuthor("");
+    setContent("");
 
-    return(
-        <>
-            <div class="input-group mb-3 mt-5">
-                <span class="input-group-text" id="inputGroup-sizing-default">Your Name or Email</span>
-                <input value={author} onChange={(event)=>{setAuthor(event.target.value)}} type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"></input>
-                
+    console.log(content);
+  };
+
+  return (
+    <>
+      <div class="input-group mb-3 mt-5">
+        <span class="input-group-text" id="inputGroup-sizing-default">
+          Your Name or Email
+        </span>
+        <input
+          value={author}
+          onChange={(event) => {
+            setAuthor(event.target.value);
+          }}
+          type="text"
+          class="form-control"
+          aria-label="Sizing example input"
+          aria-describedby="inputGroup-sizing-default"
+        ></input>
+      </div>
+      <div class="input-group mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-default">
+          Your Message
+        </span>
+        <input
+          value={content}
+          onChange={(event) => {
+            setContent(event.target.value);
+          }}
+          type="text"
+          class="form-control"
+          aria-label="Sizing example input"
+          aria-describedby="inputGroup-sizing-default"
+        ></input>
+      </div>
+
+      {validator && (
+        <div
+          className="form-alert-ops alert alert-warning alert-dismissible fade show mt-2"
+          role="alert"
+        >
+          <strong>Por favor preencha todos os campos!</strong>
+          <button
+            type="button"
+            className="btn-close"
+            data-bs-dismiss="alert"
+            aria-label="Close"
+          ></button>
+        </div>
+      )}
+
+      {success && (
+        <div
+          className="form-alert-ok alert alert-success alert-dismissible fade show mt-2"
+          role="alert"
+        >
+          <strong>Mensagem foi enviada</strong>
+        </div>
+      )}
+
+      <button onClick={sendMessage} className="btn btn-outline-primary">
+        Sent
+      </button>
+
+      {message.map((content) => {
+        return (
+          <div className="card mt-2" key={content.id}>
+            <div className="card-body">
+              <h5 className="card-title">{content.email}</h5>
+              <p className="card-text">{content.message}</p>
+              <p className="card-text">
+                <small className="text-muted">{content.created_at}</small>
+              </p>
             </div>
-            <div class="input-group mb-3">
-                <span class="input-group-text" id="inputGroup-sizing-default">Your Message</span>
-                <input value={content} onChange={(event)=>{setContent(event.target.value)}} type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"></input>
-            </div>
-
-            {validator && 
-                <div className="form-alert-ops alert alert-warning alert-dismissible fade show mt-2" role="alert">
-                    <strong>Por favor preencha todos os campos!</strong>
-                    <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
-            }
-
-            {success  && 
-                <div className="form-alert-ok alert alert-success alert-dismissible fade show mt-2" role="alert">
-                    <strong>Mensagem foi enviada</strong>
-                </div>
-            }
-
-            <button onClick={sendMessage} className="btn btn-outline-primary">
-                Sent
-            </button>
-
-            {message.map((content) => {
-                return(
-                    <div className="card mt-2" key={content.id}>
-                        <div className="card-body">
-                            <h5 className="card-title">{content.email}</h5>
-                            <p className="card-text">{content.message}</p>
-                            <p className="card-text"><small className="text-muted">{content.created_at}</small></p>
-                        </div>
-                    </div>
-                )
-            } )}
-        </>
-    )
-}
+          </div>
+        );
+      })}
+    </>
+  );
+};
 
 export default Contatos;
